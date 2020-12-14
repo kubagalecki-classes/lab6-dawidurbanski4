012b58dd250bed36bf8e8e9b6d60818c43b38423
dawidurbanski4 <urbdadylbod@gmail.com>
2020-12-14 21:06:40 +0000

    solved

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -11,6 +11,15 @@ std::vector< char > foo(std::list< Human >& people)
     std::vector< char > ret_v(people.size());
 
     // Twoja implementacja tutaj
+    for_each(people.begin(), people.end(), [&](auto& iter) { return (iter.birthday()); });
+
+    transform(people.begin(), people.end(), ret_v.rbegin(), [&](auto& iter) {
+        if (iter.isMonster() == true) {
+            return 'n';
+        } else {
+            return 'y';
+        }
+    });
 
     return ret_v;
 }
